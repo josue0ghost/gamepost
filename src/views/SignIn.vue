cd6a55064b19de22425c29422fdf50faf7d42789
Emmanuel Alvarado <josue0ghost@gmail.com>
2021-02-20 03:13:29 -0600

    log in y sign up views

---
+++ src/views/SignIn.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <!--
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    -->
+    <LogIn/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import LogIn from "@/components/LogIn.vue";
+
+@Component({
+  components: {
+    LogIn
+  }
+})
+export default class Home extends Vue {}
+</script>
