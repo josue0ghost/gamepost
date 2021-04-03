325ea417a67acf1b9320dee788ed327981a9a6dc
Emmanuel Alvarado <josue0ghost@gmail.com>
2021-04-02 19:34:53 -0600

    bugfix auth is not defined

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,119 +1,118 @@
 <template>
-    <div>
-        <b-container class="empty">
-        </b-container>
-        <b-container>
-            <div class="row align-items-center">
-                <b-col class="col-md-6 col-12 text-center">
-                    <img alt="GamePost logo" width="100%" height="100%" src="../assets/logo.png" />
-                </b-col>
-                <b-col class="col-md-6 col-12 text-center">
-                    <b-card-body class="square-card">                    
-                        <b-form role="form" @submit.prevent="onSubmit(onSubmit)">
-                            <b-form-input alternative
-                                class="my-3 col-6 custom-input-2 font-general "
-                                name="Name"
-                                :rules="{required: true}"
-                                prepend-icon="ni ni-email-83"
-                                placeholder="name"
-                                v-model="user.name"
-                                style="margin-right: 5px; margin-left: 10px;">
-                            </b-form-input>
+  <div>
+    <b-container class="empty">
+    </b-container>
+    <b-container>
+      <div class="row align-items-center">
+        <b-col class="col-md-6 col-12 text-center">
+          <img alt="GamePost logo" width="100%" height="100%" src="../assets/logo.png" />
+        </b-col>
+        <b-col class="col-md-6 col-12 text-center">
+          <b-card-body class="square-card">                    
+            <b-form role="form" @submit.prevent="onSubmit(onSubmit)">
+              <b-form-input alternative
+                class="my-3 col-6 custom-input-2 font-general "
+                name="Name"
+                :rules="{required: true}"
+                prepend-icon="ni ni-email-83"
+                placeholder="name"
+                v-model="user.name"
+                style="margin-right: 5px; margin-left: 10px;">
+              </b-form-input>
 
-                            <b-form-input alternative
-                                class="my-3 col-6 custom-input-2 font-general "
-                                name="Lastname"
-                                :rules="{required: true}"
-                                prepend-icon="ni ni-lock-circle-open"
-                                placeholder="last name"
-                                v-model="user.lastName"
-                                style="margin-right: 10px; margin-left: 5px;">
-                            </b-form-input>
-                            
-                            <b-form-input alternative
-                                class="mb-3 custom-input font-general"
-                                name="Email"
-                                :rules="{required: true, email: true}"
-                                :state="invalidEmailFeedback"
-                                prepend-icon="ni ni-lock-circle-open"
-                                placeholder="email"
-                                v-model="user.email">
-                            </b-form-input>
+              <b-form-input alternative
+                class="my-3 col-6 custom-input-2 font-general "
+                name="Lastname"
+                :rules="{required: true}"
+                prepend-icon="ni ni-lock-circle-open"
+                placeholder="last name"
+                v-model="user.lastName"
+                style="margin-right: 10px; margin-left: 5px;">
+              </b-form-input>
+              
+              <b-form-input alternative
+                class="mb-3 custom-input font-general"
+                name="Email"
+                :rules="{required: true, email: true}"
+                :state="invalidEmailFeedback"
+                prepend-icon="ni ni-lock-circle-open"
+                placeholder="email"
+                v-model="user.email">
+              </b-form-input>
 
-                            <b-form-input alternative
-                                class="mb-3 custom-input font-general"
-                                name="Password"
-                                :rules="{required: true}"
-                                :state="invalidPasswordFeedback"
-                                prepend-icon="ni ni-lock-circle-open"
-                                type="password"
-                                placeholder="password"
-                                v-model="user.password">
-                            </b-form-input>
+              <b-form-input alternative
+                class="mb-3 custom-input font-general"
+                name="Password"
+                :rules="{required: true}"
+                :state="invalidPasswordFeedback"
+                prepend-icon="ni ni-lock-circle-open"
+                type="password"
+                placeholder="password"
+                v-model="user.password">
+              </b-form-input>
 
-                            <b-form-input
-                                class="mb-3 custom-input font-general"
-                                name="BirthDate"
-                                type="date"
-                                v-model="user.birthDate">
+              <b-form-input
+                class="mb-3 custom-input font-general"
+                name="BirthDate"
+                type="date"
+                v-model="user.birthDate">
 
-                            </b-form-input>
+              </b-form-input>
 
-                            <b-form-checkbox class="my-2" v-model="user.acceptTermsAndConditions">Accept our terms and conditions</b-form-checkbox>
+              <b-form-checkbox class="my-2" v-model="user.acceptTermsAndConditions">Accept our terms and conditions</b-form-checkbox>
 
-                            <b-button type="submit" class="font-general " block variant="primary">Sign In</b-button>
-                        </b-form>
+              <b-button type="submit" class="font-general " block variant="primary">Sign In</b-button>
+            </b-form>
 
-                    </b-card-body>
-                </b-col>
-            </div>
-        </b-container>
-    </div>
+          </b-card-body>
+        </b-col>
+      </div>
+    </b-container>
+  </div>
 </template>
 
 <script>
-    export default {
-        computed: {
-            invalidEmailFeedback() {
-                return this.user.email.includes("@")
-            },
-            invalidPasswordFeedback() {
-                return this.user.password.length >= 8
-            }
+  import auth from '@/store/auth.js';
+  export default {
+    computed: {
+      invalidEmailFeedback() {
+        return this.user.email.includes("@")
+      },
+      invalidPasswordFeedback() {
+        return this.user.password.length >= 8
+      }
+    },
+    data() {
+      return {
+        user: {
+          name: '',
+          lastName: '',
+          email: '',
+          password: '',
+          birthDate: '',
+          acceptTermsAndConditions: false
         },
-        data() {
-            return {
-                user: {
-                    name: '',
-                    lastName: '',
-                    email: '',
-                    password: '',
-                    birthDate: '',
-                    acceptTermsAndConditions: false
-                },
-                signuperror: false
+        signuperror: false
+      }
+    },
+    methods: {
+      async onSubmit() {
+        // this will be called only after form is valid. You can do api call here to login
+        try {
+          await auth.signup(this.user).then((response) => {
+          console.log(response);
+            if(response.status == 200){
+              this.signuperror = false;
+              this.$router.push("/");
+            } else {
+              this.signuperror = true;
             }
-        },
-        methods: {
-            async onSubmit() {
-                // this will be called only after form is valid. You can do api call here to login
-                try {
-                    await auth.signup(this.user).then((response) => {
-                    console.log(response);
-                        if(response.status == 200){
-                            this.signuperror = false;
-                            this.$router.push("/");
-                        } else {
-                            this.signuperror = true;
-                        }
-                    });
-                } catch (err) {
-                    console.error(err);
-                    this.signuperror = true;
-                }
-                
-                
-            }
+          });
+        } catch (err) {
+          console.error(err);
+          this.signuperror = true;
         }
+      }
     }
+  }
 </script>
