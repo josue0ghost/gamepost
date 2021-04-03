9ea10adbf0494ecff29235af8cddda671e2dadc4
Emmanuel Alvarado <josue0ghost@gmail.com>
2021-04-02 19:31:21 -0600

    signup api

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -14,12 +14,12 @@
                                 class="my-3 col-6 custom-input-2 font-general "
                                 name="Name"
                                 :rules="{required: true}"
-                                :invalid-feedback="invalidFeedback"
                                 prepend-icon="ni ni-email-83"
                                 placeholder="name"
                                 v-model="user.name"
                                 style="margin-right: 5px; margin-left: 10px;">
                             </b-form-input>
+
                             <b-form-input alternative
                                 class="my-3 col-6 custom-input-2 font-general "
                                 name="Lastname"
@@ -34,19 +34,23 @@
                                 class="mb-3 custom-input font-general"
                                 name="Email"
                                 :rules="{required: true, email: true}"
+                                :state="invalidEmailFeedback"
                                 prepend-icon="ni ni-lock-circle-open"
                                 placeholder="email"
                                 v-model="user.email">
                             </b-form-input>
+
                             <b-form-input alternative
                                 class="mb-3 custom-input font-general"
                                 name="Password"
-                                :rules="{required: true, min: 6}"
+                                :rules="{required: true}"
+                                :state="invalidPasswordFeedback"
                                 prepend-icon="ni ni-lock-circle-open"
                                 type="password"
                                 placeholder="password"
                                 v-model="user.password">
                             </b-form-input>
+
                             <b-form-input
                                 class="mb-3 custom-input font-general"
                                 name="BirthDate"
@@ -70,14 +74,11 @@
 <script>
     export default {
         computed: {
-            state() {
-                return this.user.email.length >= 4
+            invalidEmailFeedback() {
+                return this.user.email.includes("@")
             },
-            invalidFeedback() {
-                if (this.user.email.length > 0) {
-                    return 'Enter at least 4 characters.'
-                }
-                return 'T'
+            invalidPasswordFeedback() {
+                return this.user.password.length >= 8
             }
         },
         data() {
@@ -89,18 +90,29 @@
                     password: '',
                     birthDate: '',
                     acceptTermsAndConditions: false
-                }
+                },
+                signuperror: false
             }
         },
         methods: {
-            onSubmit() {
+            async onSubmit() {
                 // this will be called only after form is valid. You can do api call here to login
+                try {
+                    await auth.signup(this.user).then((response) => {
+                    console.log(response);
+                        if(response.status == 200){
+                            this.signuperror = false;
+                            this.$router.push("/");
+                        } else {
+                            this.signuperror = true;
+                        }
+                    });
+                } catch (err) {
+                    console.error(err);
+                    this.signuperror = true;
+                }
                 
-                console.log(this.user.name);
-                console.log(this.user.lastName);
-                console.log(this.user.email);
-                console.log(this.user.password);
-                console.log(this.user.birthDate);
+                
             }
         }
     }
--- src/store/auth.js
+++ src/store/auth.js
@@ -7,5 +7,8 @@ Vue.use(VueAxios, axios)
 export default {
     login(user){
         return Vue.axios.post('https://y38x8sqx2b.execute-api.us-east-2.amazonaws.com/test/auth', user);
-    }   
+    },
+    signup(user){
+        return Vue.axios.put('https://y38x8sqx2b.execute-api.us-east-2.amazonaws.com/test/auth', user);
+    }
 }
